365a6f43014d99e4f08baa403a9a40a412f5393b
reubenbubu <reuben.abela@gmail.com>
2013-09-10 17:55:45 +0200

    Method call queueing. First attempt.

--- clessidra/src/main/java/com/ccbill/clessidra/aspect/BaseRateLimiter.java
+++ clessidra/src/main/java/com/ccbill/clessidra/aspect/BaseRateLimiter.java
@@ -1,6 +1,10 @@
 package com.ccbill.clessidra.aspect;
 
+
+import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 import org.apache.log4j.Logger;
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -10,93 +14,196 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
 import com.ccbill.clessidra.annotations.RateLimited;
+import com.ccbill.clessidra.annotations.RateLimitedQueue;
 import com.ccbill.clessidra.exception.RateLimiterException;
 import com.ccbill.clessidra.strategy.AbstractLimiterStrategy;
 import com.ccbill.clessidra.strategy.LimiterStrategyConclusion;
 
+
+
 /**
  * 
- * {@link BaseRateLimiter} holds the core logic of when a method call is intercepted for rate limiting.
+ * {@link BaseRateLimiter} holds the core logic of when a method call is intercepted for rate
+ * limiting.
  * 
  * @author reubena
  * 
  */
-public class BaseRateLimiter implements ApplicationContextAware {
+public class BaseRateLimiter
+        implements ApplicationContextAware {
 
-	private Logger logger = Logger.getLogger(BaseRateLimiter.class);
+    private Logger logger = Logger.getLogger(BaseRateLimiter.class);
 
-	private ApplicationContext applicationContext;
+    private ApplicationContext applicationContext;
 
-	public BaseRateLimiter() {
-	}
+    private static Map<String, ConcurrentLinkedQueue<Thread>> threadQueue = new ConcurrentHashMap<String, ConcurrentLinkedQueue<Thread>>();
 
-	/**
-	 * The core Rate Limiter logic. This method will call the limiter strategy chain and decide whether to annotated method will run or not.
-	 * 
-	 * @param joinPoint
-	 * @return The object returned by the annotated method
-	 * @throws Throwable
-	 */
-	public Object intercept(ProceedingJoinPoint joinPoint) throws Throwable {
 
-		// extract MethodSignature
-		MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
-		logger.debug("Intercepted a method call on " + methodSignature.toString());
 
-		// find the RateLimited annotation, method gets priority over class
-		RateLimited annotation = methodSignature.getMethod().getAnnotation(RateLimited.class);
-		if (annotation == null) {
-			annotation = (RateLimited) joinPoint.getSourceLocation().getWithinType().getAnnotation(RateLimited.class);
-		}
+    public BaseRateLimiter() {
+    }
 
-		// resolve method group name based on the methodGrouping in the annotation
-		String methodGroupName = null;
-		switch (annotation.methodGrouping()) {
-		case GROUPED:
-			methodGroupName = annotation.groupName();
-			break;
-		case UNGROUPED:
-			methodGroupName = methodSignature.toString();
-			break;
-		default:
-			methodGroupName = methodSignature.toString();
-			break;
-		}
 
-		// get the limiter strategy chain from the spring context
-		AbstractLimiterStrategy limiterStrategyChain = (AbstractLimiterStrategy) applicationContext.getBean(annotation.limiterBean());
 
-		// generate a uuid to be used later on if rollback is required
-		UUID methodInvocationUUID = UUIDGenerator.generateUUID();
+    /**
+     * The core Rate Limiter logic. This method will call the limiter strategy chain and decide
+     * whether to annotated method will run or not.
+     * 
+     * @param joinPoint
+     * @return The object returned by the annotated method
+     * @throws Throwable
+     */
+    public Object intercept(ProceedingJoinPoint joinPoint)
+            throws Throwable {
 
-		// run through the limiter strategy chain and get the conclusion
-		LimiterStrategyConclusion conclusion = limiterStrategyChain.hasLimitBeenExceededChain(methodGroupName, methodInvocationUUID,
-				joinPoint.getArgs());
+        // extract MethodSignature
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        logger.debug("Intercepted a method call on " + methodSignature.toString());
 
-		// if limit is not exceeded call the method and do a post invocation clean up where needed
-		if (!conclusion.getHasLimitBeenExceeded()) {
-			logger.trace("About to run method " + methodSignature.toString());
-			try {
-				Object obj = joinPoint.proceed(joinPoint.getArgs());
-				logger.trace("Finished running " + methodSignature.toString());
-				return obj;
-			} catch (Throwable t) {
-				logger.debug("Method " + methodSignature.toString() + " did not exit gracefully. " + t);
-				throw t;
-			} finally {
-				limiterStrategyChain.postInvocationCleanupChain(methodGroupName, methodInvocationUUID, joinPoint.getArgs());
-			}
-		}
+        // find the RateLimited annotation, method gets priority over class
+        RateLimited rateLimitedAnnotation = methodSignature.getMethod().getAnnotation(RateLimited.class);
+        if (rateLimitedAnnotation == null) {
+            rateLimitedAnnotation = (RateLimited) joinPoint.getSourceLocation().getWithinType().getAnnotation(RateLimited.class);
+        }
 
-		// throw an exception containing the conclusion
-		throw new RateLimiterException("Method call blocked.", conclusion);
-	}
+        // resolve method group name based on the methodGrouping in the annotation
+        String methodGroupName = null;
+        switch (rateLimitedAnnotation.methodGrouping()) {
+            case GROUPED:
+                methodGroupName = rateLimitedAnnotation.groupName();
+                break;
+            case UNGROUPED:
+                methodGroupName = methodSignature.toString();
+                break;
+            default:
+                methodGroupName = methodSignature.toString();
+                break;
+        }
 
-	/**
-	 * Spring will automagically set the application context using this method thanks to {@link ApplicationContextAware}
-	 */
-	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-		this.applicationContext = applicationContext;
-	}
+        // get the queueing configuration
+
+        RateLimitedQueue rateLimitedQueueAnnotation = methodSignature.getMethod().getAnnotation(RateLimitedQueue.class);
+        if (rateLimitedQueueAnnotation == null) {
+            rateLimitedQueueAnnotation = (RateLimitedQueue) joinPoint.getSourceLocation().getWithinType().getAnnotation(RateLimitedQueue.class);
+        }
+
+        ConcurrentLinkedQueue<Thread> queue = null;
+
+        if (rateLimitedAnnotation != null) {
+
+            boolean sleepEternally = false;
+
+            synchronized (BaseRateLimiter.class) {
+                queue = threadQueue.get(methodGroupName);
+                if (queue == null) {
+                    queue = new ConcurrentLinkedQueue<Thread>();
+                    threadQueue.put(methodGroupName, queue);
+                    logger.debug("Created queue");
+                }
+
+                if (queue.peek() != null) {
+                    if (queue.size() < rateLimitedQueueAnnotation.queueSize()) {
+                        queue.offer(Thread.currentThread());
+                        logger.debug("Put thread " + Thread.currentThread().getName() + " in queue");
+                        sleepEternally = true;
+                    }
+                    else {
+                        logger.debug("Thread " + Thread.currentThread().getName() + " can't fit in the queue.");
+                    }
+                }
+            }
+
+            if (sleepEternally) {
+                try {
+                    Thread.sleep(Long.MAX_VALUE);
+                }
+                catch (InterruptedException ignore) {
+                }
+                logger.debug("Awakened thread " + Thread.currentThread().getName() + " from eternal sleep.");
+            }
+
+        }
+
+        // get the limiter strategy chain from the spring context
+        AbstractLimiterStrategy limiterStrategyChain = (AbstractLimiterStrategy) applicationContext.getBean(rateLimitedAnnotation.limiterBean());
+
+        // generate a uuid to be used later on if rollback is required
+        UUID methodInvocationUUID = UUIDGenerator.generateUUID();
+
+
+        LimiterStrategyConclusion conclusion;
+
+
+        if (rateLimitedQueueAnnotation == null) {
+
+            // run through the limiter strategy chain and get the conclusion
+            conclusion = limiterStrategyChain.hasLimitBeenExceededChain(methodGroupName, methodInvocationUUID, joinPoint.getArgs());
+        }
+        else {
+
+            do {
+                conclusion = limiterStrategyChain.hasLimitBeenExceededChain(methodGroupName, methodInvocationUUID, joinPoint.getArgs());
+                if (conclusion.getHasLimitBeenExceeded()) {
+                    if (!queue.contains(Thread.currentThread())) {
+                        if (queue.size() < rateLimitedQueueAnnotation.queueSize()) {
+                            queue.offer(Thread.currentThread());
+                            logger.debug("Put thread " + Thread.currentThread().getName() + " in queue");
+                            try {
+                                Thread.sleep(Long.MAX_VALUE);
+                            }
+                            catch (InterruptedException ignore) {
+                            }
+                            logger.debug("Awakened thread " + Thread.currentThread().getName() + " from eternal sleep");
+                        }
+                        else {
+                            logger.debug("Thread " + Thread.currentThread().getName() + " can't fit in the queue.");
+                            break;
+                        }
+                    }
+                }
+
+            } while (conclusion.getHasLimitBeenExceeded());
+
+        }
+
+
+        // if limit is not exceeded call the method and do a post invocation clean up where needed
+        if (!conclusion.getHasLimitBeenExceeded()) {
+            logger.trace("About to run method " + methodSignature.toString());
+            try {
+                Object obj = joinPoint.proceed(joinPoint.getArgs());
+                logger.trace("Finished running " + methodSignature.toString());
+                return obj;
+            }
+            catch (Throwable t) {
+                logger.debug("Method " + methodSignature.toString() + " did not exit gracefully. " + t);
+                throw t;
+            }
+            finally {
+                limiterStrategyChain.postInvocationCleanupChain(methodGroupName, methodInvocationUUID, joinPoint.getArgs());
+                synchronized (this) {
+                    if (queue != null && queue.peek() != null) {
+                        Thread t = queue.poll();
+                        logger.debug("Interrupting " + t.getName());
+                        t.interrupt();
+                    }
+                }
+            }
+        }
+
+        // throw an exception containing the conclusion
+        throw new RateLimiterException("Method call blocked.", conclusion);
+    }
+
+
+
+    /**
+     * Spring will automagically set the application context using this method thanks to
+     * {@link ApplicationContextAware}
+     */
+    public void setApplicationContext(ApplicationContext applicationContext)
+            throws BeansException {
+        this.applicationContext = applicationContext;
+    }
 
 }
---
+++ clessidra/src/test/java/com/ccbill/clessidra/tests/QueueTests.java
@@ -0,0 +1,62 @@
+package com.ccbill.clessidra.tests;
+
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Vector;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.ccbill.clessidra.tests.services.ConcurrencyQueuedServiceMethodAnnotated;
+
+
+
+@ContextConfiguration(locations = { "classpath:beans.clessidra.test.xml" })
+@RunWith(SpringJUnit4ClassRunner.class)
+public class QueueTests {
+
+
+    @Autowired
+    private ConcurrencyQueuedServiceMethodAnnotated concurrencyQueuedServiceMethodAnnotated;
+
+
+
+    @Test
+    public void testQueue()
+            throws InterruptedException {
+
+        List<Future<String>> futures = Collections.synchronizedList(new Vector<Future<String>>());
+
+
+        for (int i = 0; i < 5; i++) {
+            futures.add(concurrencyQueuedServiceMethodAnnotated.testConcurrencyUngrouped());
+        }
+        Thread.sleep(1000);
+        for (int i = 0; i < 5; i++) {
+            futures.add(concurrencyQueuedServiceMethodAnnotated.testConcurrencyUngrouped());
+            Thread.sleep(100);
+        }
+
+        for (Future<String> current : futures) {
+            try {
+                current.get();
+            }
+            catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            catch (ExecutionException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+}
