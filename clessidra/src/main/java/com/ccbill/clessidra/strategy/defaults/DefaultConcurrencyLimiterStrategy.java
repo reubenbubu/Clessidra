ebb84bf9c4a568ee7f6c7991622458312b2f454f
valeriop <valeriop@MTPC-VALERIOP.malta.ccbill-hq.local>
2018-09-25 16:08:58 +0200

    minor

--- clessidra/src/main/java/com/ccbill/clessidra/strategy/defaults/DefaultConcurrencyLimiterStrategy.java
+++ clessidra/src/main/java/com/ccbill/clessidra/strategy/defaults/DefaultConcurrencyLimiterStrategy.java
@@ -5,18 +5,19 @@ import com.ccbill.clessidra.strategy.BaseConcurrencyLimiterStrategy;
 /**
  * A useable {@link BaseConcurrencyLimiterStrategy} that groups all the
  * invocations under the same strategy group.
- * 
+ *
  * @author reubena
- * 
+ *
  */
 public class DefaultConcurrencyLimiterStrategy extends BaseConcurrencyLimiterStrategy {
 
-	public DefaultConcurrencyLimiterStrategy(int concurrencyLimit) {
-		super(concurrencyLimit);
-	}
+    public DefaultConcurrencyLimiterStrategy(int concurrencyLimit) {
+        super(concurrencyLimit);
+    }
 
-	public String getStrategyGroupKey(Object[] args) {
-		return "";
-	}
+    @Override
+    public String getStrategyGroupKey(Object[] args) {
+        return "";
+    }
 
 }
